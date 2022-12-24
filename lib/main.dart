4632043a1bcc60bf97649e601a423e34d2fccfc8
Ahmadjon Safarov <ahmadjonsafarov1112@gmail.com>
2022-12-24 23:54:30 +0500

    Home PAge

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:name/page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-
+      home: Home_Page(),
     );
   }
 }
